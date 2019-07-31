e608b530429a741c8e1b39a43260e6e79bb8b86a
chaitrasarathy <chaitrasarathy@gmail.com>
2019-07-31 14:46:49 +0200

    updated scripts for efm selection

--- core/MATLAB/createResultFolder.m
+++
@@ -1,9 +0,0 @@
-function resultPath = createResultFolder(resultFolder,fileSeparator, useCaseFolder, workfloStepFolder)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-resultPath = [resultFolder fileSeparator useCaseFolder fileSeparator workfloStepFolder]; 
-if (exist(resultPath, 'dir') == 0), mkdir(resultPath); end
-
-end
-
